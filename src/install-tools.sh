3e792bd75786e6c847db0c03e0df74cb8d77dcf0
hinkman <hinkman@hinkman.com>
2014-05-24 18:08:03 -0700

    Update install-tools.sh

--- src/install-tools.sh
+++ src/install-tools.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-apt-get install -qq dnsutils
-apt-get install -qq mtr
+DATE=${DATE:-`date +%Y%m%d%H%M%S`}
+LOG=${LOG:-/tmp/install-tools-$DATE.log}
+
+apt-get install -y dnsutils > ${LOG} 2>&1
+apt-get install -y mtr  > ${LOG} 2>&1
