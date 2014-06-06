02b547072b04342a99324daea4679bc38ad3c1db
Mark L. Hinkle <hinkman@hinkman.com>
2014-06-06 17:18:30 -0400

    postgres added

--- src/install-tools.sh
+++ src/install-tools.sh
@@ -5,3 +5,9 @@ LOG=${LOG:-/tmp/install-tools-$DATE.log}
 
 apt-get install -y dnsutils >> ${LOG} 2>&1
 apt-get install -y mtr  >> ${LOG} 2>&1
+apt-get install -y krb5-{admin-server,kdc}
+apt-get install -y libpam-krb5
+cat >> /etc/apt/sources.list <EOF
+deb http://ftp.de.debian.org/debian sid main
+deb http://ftp.de.debian.org/debian experimental main
+EOF
