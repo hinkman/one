a6231dea534ce5d286ccc6da2ab4ed35e12819d3
Mark L. Hinkle <hinkman@hinkman.com>
2014-06-13 08:18:37 -0700

    updated nameserver steps

--- src/install-nameserver.sh
+++ src/install-nameserver.sh
@@ -7,7 +7,6 @@ echo "deb http://ftp.de.debian.org/debian sid main" >> /etc/apt/sources.list
 echo "deb http://ftp.de.debian.org/debian experimental main" >> /etc/apt/sources.list
 apt-get update >> ${LOG} 2>&1
 apt-get install -y dnscache-run  >> ${LOG} 2>&1
-apt-get install -y tinydns-run  >> ${LOG} 2>&1
 echo "1" > /etc/dnscache/env/FORWARDONLY
 LOCALIP=`ifconfig -a | head -2 | tail -1 | sed 's/.*addr:\([0-9\.]*\) .*$/\1/'`
 echo ${LOCALIP} > /etc/dnscache/env/IP
@@ -19,4 +18,16 @@ if [ -z ${LOCALDOMAIN} ]; then
 fi
 LOCALDOMAIN=${LOCALDOMAIN:-"localdomain.com"}
 echo "127.0.0.1" > /etc/dnscache/root/servers/${LOCALDOMAIN}
+
+# resolv.conf
+# domain ${LOCALDOMAIN}
+# search ${LOCALDOMAIN} existing...
+# echo "nameserver ${LOCALIP}" >> /etc/resolv.conf
+
+
+apt-get install -y tinydns-run  >> ${LOG} 2>&1
 echo "127.0.0.1" > /etc/tinydns/env/IP
+echo "&hinkman.com::dev-admin01.hinkman.com.:3600" >> /etc/tinydns/root/data
+echo ".hinkman.com:127.0.0.1:a:3600" >> /etc/tinydns/root/data
+echo "=dev-admin01.hinkman.com.:10.240.99.77:3600" >> /etc/tinydns/root/data
+echo "=robot.hinkman.com.:10.240.69.249:3600" >> /etc/tinydns/root/data
